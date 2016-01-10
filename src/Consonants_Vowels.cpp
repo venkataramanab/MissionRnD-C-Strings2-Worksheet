0ebe7090aa0d5252b5dd30b78475a990d2d1930a
venkataramanab <venkataramanab@missionrnd.com>
2016-01-10 16:42:12 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -21,6 +21,16 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
+	*consonants = 0;
+	*vowels = 0;
+	if (str){
+		for (int i = 0; str[i] != NULL; i++){
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
+				*vowels += 1;
+			}
+			else if (((str[i] > 'a') && (str[i] <= 'z')) || ((str[i] > 'A') && (str[i] <= 'Z'))){
+				*consonants += 1;
+			}
+		}
+	}
 }
